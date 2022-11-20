ab14b9dcb6c35b972c47d85ba8370836cac443fa
Matheus Almeida <92039521+MatheusAlmeida28@users.noreply.github.com>
2022-11-20 15:11:03 -0300

    Correção nos espaços

--- models.py
+++ models.py
@@ -3,8 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-
-
 CONNECTION = "sqlite:///teste_login.db"
 
 engine = create_engine(CONNECTION, echo=True)
